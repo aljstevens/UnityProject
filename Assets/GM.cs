7806f6a64b23359345ed0090d5c7b69f63010065
Alexander Stevens <Alexander Stevens>
2016-07-04 14:55:00 -0400

    First Commit

---
+++ Assets/GM.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GM : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Log ("Rump");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
